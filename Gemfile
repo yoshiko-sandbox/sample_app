bb1cca3bb21ff54f57e369b4beac32192dab3565
maasa <maasa.giss2@gmail.com>
2014-05-05 00:40:23 +0900

    Add user microposts

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ gem 'bootstrap-sass', '2.1'
 gem 'bcrypt-ruby', '3.0.1'
 gem 'faker', '1.0.1'
 gem 'will_paginate',  '3.0.3'
+gem 'will_paginate-bootstrap'
 gem 'pg', '0.12.2'
 
 group :development, :test do
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -21,7 +21,7 @@ describe "User pages" do
       before(:all) { 30.times { FactoryGirl.create(:user) } }
       after(:all) { User.delete_all }
 
-      it { should have_selector('div.pagination') }
+      it { should have_selector('ul.pagination') }
 
       it "should list each user" do
         User.paginate(page: 1).each do |user|
@@ -56,10 +56,19 @@ describe "User pages" do
 
   describe "profile page" do
     let(:user) {FactoryGirl.create(:user) }
+    let!(:m1) { FactoryGirl.create(:micropost,  user: user, content: "Foo") }
+    let!(:m2) { FactoryGirl.create(:micropost,  user: user, content: "Bar") }
+
     before { visit user_path(user) }
 
     it { should have_selector('h1', text: user.name) }
     it { should have_selector('title', text: user.name) }
+
+    describe "microposts" do
+      it { should have_content(m1.content) }
+      it { should have_content(m2.content) }
+      it { should have_content(user.microposts.count) }
+    end
   end
 
   describe "GET /user_pages" do
